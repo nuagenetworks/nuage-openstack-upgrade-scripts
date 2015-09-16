e0a33069688566dbd9cb197093642a976eb20771
cshahani <chirag.shahani@alcatel-lucent.com>
2015-09-16 15:14:23 -0700

    Added script for uninstall nuage folder cr8ed by setup.py install
    For the icehouse release we provide a tar.gz along with a setup.py
    script which the user runs to install the nuage specfiv icehouse
    folder under the python path using setup.py install. Now, during
    migration to kilo, the admins will do a apt-get upgrade of the
    neutron server. This results in a dpkg warning for the nuage
    specific folder since the package manager is unaware of the this
    nuage folder created by setup.py install. This script will basically
    do the opposite of setup.py install. That is it will find the path
    where neutron is installed, find the nuage path and delete the nuage
    folder. Run this script before doing an apt-get upgrade of the
    neutron.
    
    Change-Id: Ie8405f3e6e8fc10b554a15b8a1d783ef096c109b
    Closes-Bug: Openstack-930

---
+++ uninstall_icehouse_nuage.py
@@ -0,0 +1,40 @@
+# Copyright 2015 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+import os
+import shutil
+import sys
+
+
+def main():
+    try:
+        import neutron
+    except ImportError:
+        print ("Openstack neutron is not installed.\n"
+               " The uninstallation will stop.")
+        sys.exit(1)
+    else:
+        neutron_path = neutron.__path__[0]
+        nuage_path = os.path.join(neutron_path, 'plugins/nuage')
+        if not os.access(neutron.__path__[0], os.W_OK):
+            print ("The user does not have sufficient privileges to modify "
+                   "%s.\n The uninstallation will stop." % (neutron_path))
+
+            sys.exit(1)
+        if os.path.exists(nuage_path):
+            shutil.rmtree(nuage_path)
+
+if __name__ == '__main__':
+    main()
