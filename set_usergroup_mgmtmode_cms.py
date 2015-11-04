a66cb65e0f53cab2c5b43a6214b4c85220940363
Wim De Clercq <Wim.De_Clercq@alcatel-lucent.com>
2015-11-04 09:01:01 +0100

    Script to support setting override flag on user/group resource
    Customer trying to use VSD's LDAP feature along with Openstack in
    advance mode, will need to run this script prior to turning on that
    feature and after upgrading their VSD to 3.2R5
    
    This script should work with following combination of upgrade:
    1. Icehouse + 3.0Rx/3.2R(1-4) -> Icehouse + 3.2R5
    2. Icehouse + 3.0Rx/3.2R(1-4) -> Juno + 3.2R5
    3. Icehouse + 3.0Rx/3.2R(1-4) -> Kilo + 3.2R5
    4. Juno + 3.0x/3.2R(1-4) -> Juno + 3.2R5
    5. Juno + 3.0x/3.2R(1-4) -> Kilo + 3.2R5
    6. Kilo + 3.2R(1-4) -> Kilo + 3.2R5
    
    Script at this point can be run independantly. It can be incorporated
    into a umbreall upgrade script as well; as and when it happens.
    
    Both user and group's 'managementMode' attribute are expected to be changed
    from default 'ANY' to 'CMS' once the script is run.
    
    This operation only makes sense for the advanced subnet. But it will be performed
    for normal subnet as well.
    
    The way it works is that we walk through all the subnets that we have registered
    in VSD (L2domain and Subnet). In the database table subnet_l2dom_mapping we keep
    track of nuage user and group id for them. We do a PUT operation on all of them.
    First User, than Group.
    
    One thing that is not captured and will not work:
    When router is created in openstack, l3domain and zone(s) are created and corresponding
    tenant is mapped to user/group in the private zone in VSD. Via this script they are not
    going to get updated unless they have subnet attached to it.
    
    Change-Id: I9a8cc107c86af9bc3d35d0d3cb8fe6171a2f553a

---
+++ set_usergroup_mgmtmode_cms.py
@@ -0,0 +1,148 @@
+# Copyright 2015 Alcatel-Lucent USA Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import argparse
+import json
+import logging
+import os
+
+try:
+    from oslo.config import cfg
+except ImportError:
+    from oslo_config import cfg
+
+try:
+    from neutron.openstack.common import importutils
+except ImportError:
+    from oslo_utils import importutils
+
+from neutron.common import config
+from neutron import context as ncontext
+try:
+    from neutron.plugins.nuage.common import config as nuage_config
+except ImportError:
+    from nuage_neutron.plugins.nuage.common import config as nuage_config
+try:
+    from neutron.plugins.nuage import nuage_models
+except ImportError:
+    from nuage_neutron.plugins.nuage import nuage_models
+
+LOG = logging.getLogger('usergroup_mgmtmode')
+REST_SUCCESS_CODES = range(200, 207)
+VSD_NO_ATTR_CHANGES_TO_MODIFY_ERR_CODE = '2039'
+
+
+def set_usergroup_mgmtmode_to_cms(nuageclient):
+    context = ncontext.get_admin_context()
+    data = {'managementMode': 'CMS'}
+    query = context.session.query(nuage_models.SubnetL2Domain)
+    # Get all the VSD managed subnet
+    subnets = query.all()
+    for subnet in subnets:
+        # Get the corresponding neutron subnet object for tenant info
+        result = nuageclient.restproxy.rest_call(
+            'PUT',
+            '/users/' + subnet['nuage_user_id'],
+            data)
+        if result[0] in REST_SUCCESS_CODES:
+            LOG.debug('ManagementMode attribute successfully '
+                      'updated to CMS for user %s', subnet['nuage_user_id'])
+        else:
+            if result[0] == 409:
+                errors = json.loads(result[3])
+                error_code = str(errors.get('internalErrorCode', None))
+                if error_code == VSD_NO_ATTR_CHANGES_TO_MODIFY_ERR_CODE:
+                    LOG.debug('No change were required for user %s',
+                              subnet['nuage_user_id'])
+            else:
+                LOG.debug('Got %s response (ERROR) for user %s',
+                          result[0], subnet['nuage_user_id'])
+
+        result = nuageclient.restproxy.rest_call(
+            'PUT',
+            '/groups/' + subnet['nuage_group_id'],
+            data)
+        if result[0] in REST_SUCCESS_CODES:
+            LOG.debug('ManagementMode attribute successfully '
+                      'updated to CMS for group %s', subnet['nuage_group_id'])
+        else:
+            if result[0] == 409:
+                errors = json.loads(result[3])
+                error_code = str(errors.get('internalErrorCode', None))
+                if error_code == VSD_NO_ATTR_CHANGES_TO_MODIFY_ERR_CODE:
+                    LOG.debug('No change were required for group %s',
+                              subnet['nuage_group_id'])
+            else:
+                LOG.debug('Got %s response (ERROR) for group %s',
+                          result[0], subnet['nuage_group_id'])
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config-file", nargs='+',
+                        help='List of config files separated by space')
+    args = parser.parse_args()
+
+    conffiles = args.config_file
+    if conffiles is None:
+        parser.print_help()
+        return
+
+    # Create a logfile
+    log_dir = os.path.expanduser('~') + '/nuageupgrade'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    hdlr = logging.FileHandler(log_dir + '/usergroup_mgmtmode.log')
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    hdlr.setFormatter(formatter)
+    LOG.addHandler(hdlr)
+    logging.basicConfig(level=logging.DEBUG)
+
+    conf_list = []
+    for conffile in conffiles:
+        if not os.path.isfile(conffile):
+            LOG.error('File "%s" cannot be found.' % conffile)
+            return
+        conf_list.append('--config-file')
+        conf_list.append(conffile)
+
+    config.init(conf_list)
+    nuage_config.nuage_register_cfg_opts()
+
+    server = cfg.CONF.RESTPROXY.server
+    serverauth = cfg.CONF.RESTPROXY.serverauth
+    serverssl = cfg.CONF.RESTPROXY.serverssl
+    base_uri = cfg.CONF.RESTPROXY.base_uri
+    auth_resource = cfg.CONF.RESTPROXY.auth_resource
+    organization = cfg.CONF.RESTPROXY.organization
+
+    nuageclientinst = importutils.import_module('nuagenetlib.nuageclient')
+    try:
+        nuageclient = nuageclientinst.NuageClient(server=server,
+                                                  base_uri=base_uri,
+                                                  serverssl=serverssl,
+                                                  serverauth=serverauth,
+                                                  auth_resource=auth_resource,
+                                                  organization=organization)
+    except Exception as e:
+        LOG.error("Error in connecting to VSD:%s", str(e))
+        return
+
+    set_usergroup_mgmtmode_to_cms(nuageclient)
+    LOG.debug("Script to set User and Group's mgmtmode to CMS completed")
+
+if __name__ == '__main__':
+    main()
