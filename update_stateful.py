330af5bfc81d34df62e4496bbb8ca0b3b3cfaca8
Divya ChanneGowda <divya.channegowda@alcatel-lucent.com>
2016-03-14 18:12:01 -0700

    Fixes PROD-2253 In 3.2, setting ICMPType and ICMPCode in ICMP rule was not allowed. But neutron allows to set these fields and in 3.2, we ignored these and set it to None. As 4.0 VSP supports ICMPType and ICMPCode, upgrade script will: 1. if ICMPType is None, do nothing 2. if ICMPType is in [8,13,15,17], update the stateful flag to true and ICMPType and ICMPCode with valid values and delete the other direction extra icmp rule 3. if ICMPType is not in [8,13,15,17], update the ICMPType and ICMPCode
    Change-Id: Ica2ff947e873f02ee3e0f8db8414b87df65eeb3a

--- update_stateful.py
+++ update_stateful.py
@@ -38,6 +38,7 @@ from nuage_neutron.plugins.common import config as nuage_config
 
 LOG = logging.getLogger('Upgrade_Logger')
 REST_SUCCESS_CODES = range(200, 207)
+STATEFUL_ICMP_TYPES = [8, 13, 15, 17]
 
 
 class UpdatePGRules(db_base_plugin_v2.NeutronDbPluginV2,
@@ -53,17 +54,24 @@ class UpdatePGRules(db_base_plugin_v2.NeutronDbPluginV2,
         errors = json.loads(responsedata)
         return str(errors['errors'][0]['descriptions'][0]['description'])
 
-    def validate(self, response):
+    def validate(self, response, action, vsd_rule_id, neutron_rule_id):
         if response[0] not in REST_SUCCESS_CODES:
             err_msg = self.get_error_msg(response[3])
             LOG.error(err_msg)
             return False
         else:
+            LOG.info("Action %(action)s on ACLrule %(rule_id)s (with "
+                     "neutron external id %(neutron_rule_id)s) is "
+                     "successful" % {'action': action,
+                                     'rule_id': vsd_rule_id,
+                                     'neutron_rule_id': neutron_rule_id})
             return True
+
 
     def update_pg_rules(self):
         self.update_proto_any_rules()
         #removing icmp update, as icmp behavior is same as 3.2 in 4.0
+        self.update_icmp_rules()
 
     def update_proto_any_rules(self):
         # Get all rules on VSD with externelID set to neutron 'ANY' rule
@@ -121,13 +129,16 @@ class UpdatePGRules(db_base_plugin_v2.NeutronDbPluginV2,
                                     response = (
                                         self.nuageclient.restproxy.rest_call(
                                             'PUT', update_in_rule, data=data))
-                                    self.validate(response)
+                                    self.validate(response, 'PUT', rule,
+                                                  sgrule['id'])
                                 else:
                                     #delete the remaining rules with the
                                     # same neutron ANY rule id as external ID
                                     response = (
                                         self.nuageclient.restproxy.rest_call(
                                             'DELETE', update_in_rule, ''))
+                                    self.validate(response, 'DELETE', rule,
+                                                  sgrule['id'])
                     if eg_response[3]:
                         # these are the extra ICMP rules created in opposite
                         #  direction. Delete all these ICMP rules.
@@ -141,6 +152,8 @@ class UpdatePGRules(db_base_plugin_v2.NeutronDbPluginV2,
                                               rule+"?responseChoice=1")
                             response = self.nuageclient.restproxy.rest_call(
                                 'DELETE', delete_eg_rule, '')
+                            self.validate(response, 'DELETE', rule,
+                                          sgrule['id'])
                 elif sgrule['direction'] == 'ingress':
                     if eg_response[3]:
                         acllist = eg_response[3]
@@ -168,7 +181,16 @@ class UpdatePGRules(db_base_plugin_v2.NeutronDbPluginV2,
                                     response = (
                                         self.nuageclient.restproxy.rest_call(
                                             'PUT', update_eg_rule, data=data))
-                                    self.validate(response)
+                                    self.validate(response, 'PUT', rule,
+                                                  sgrule['id'])
+                                else:
+                                    #delete the remaining rules with the
+                                    # same neutron ANY rule id as external ID
+                                    response = (
+                                        self.nuageclient.restproxy.rest_call(
+                                            'DELETE', update_eg_rule, ''))
+                                    self.validate(response, 'DELETE', rule,
+                                                  sgrule['id'])
                     if in_response[3]:
                         acllist = in_response[3]
                         for key, group in itertools.groupby(
@@ -182,6 +204,149 @@ class UpdatePGRules(db_base_plugin_v2.NeutronDbPluginV2,
                                 response = (
                                     self.nuageclient.restproxy.rest_call(
                                         'DELETE', delete_in_rule, ''))
+                                self.validate(response, 'DELETE', rule,
+                                              sgrule['id'])
+            except Exception as e:
+                LOG.error(str(e))
+
+
+    def update_icmp_rules(self):
+        sgrules = sg_rules = self.get_security_group_rules(
+                        self.context,
+                        {'protocol': ['icmp']})
+        for sgrule in sgrules:
+            try:
+                ingress_url_str = ("/ingressaclentrytemplates" +
+                                   "?responseChoice=1")
+                egress_url_str = ("/egressaclentrytemplates"+
+                                  "?responseChoice=1")
+                headers = {}
+                extra_headers = "externalID IS '%s@%s'" % (sgrule['id'],
+                                                           self.cms_id)
+                headers['X-Nuage-Filter'] = extra_headers
+
+                in_response = self.nuageclient.restproxy.rest_call(
+                    'GET', ingress_url_str, '', extra_headers=headers)
+                eg_response = self.nuageclient.restproxy.rest_call(
+                    'GET', egress_url_str, '', extra_headers=headers)
+                update_rule_list = []
+                delete_rule_list = []
+
+                if sgrule['direction'] == 'egress':
+                    if in_response[3]:
+                        acllist = in_response[3]
+                        for key, group in itertools.groupby(
+                                acllist, lambda item: item["parentID"]):
+                            for item in group:
+                                update_rule_list.append(item['ID'])
+                    if eg_response[3]:
+                        acllist = eg_response[3]
+                        for key, group in itertools.groupby(
+                                acllist, lambda item: item["parentID"]):
+                            for item in group:
+                                delete_rule_list.append(item['ID'])
+
+                    for rule in update_rule_list:
+                        # rules with ICMPCode and ICMPType None
+                        # do nothing
+                        update_in_rule =  ("/ingressaclentrytemplates/"+rule+
+                                           "?responseChoice=1")
+                        if (not sgrule['port_range_min']):
+                            continue
+                        elif (sgrule['port_range_min'] in STATEFUL_ICMP_TYPES):
+                            data = {
+                                "stateful": True,
+                                "ICMPType": sgrule['port_range_min'],
+                                "ICMPCode": sgrule['port_range_max']
+                            }
+                            response = self.nuageclient.restproxy.rest_call(
+                                'PUT', update_in_rule, data=data)
+                            self.validate(response, 'PUT', rule, sgrule['id'])
+                        elif (sgrule['port_range_min'] not in
+                                  STATEFUL_ICMP_TYPES):
+                            data = {
+                                "ICMPType": sgrule['port_range_min'],
+                                "ICMPCode": sgrule['port_range_max']
+                            }
+                            response = self.nuageclient.restproxy.rest_call(
+                                'PUT', update_in_rule, data=data)
+                            self.validate(response, 'PUT', rule, sgrule['id'])
+
+                    for rule in delete_rule_list:
+                        delete_eg_rule = ("/egressaclentrytemplates/"+rule+
+                                          "?responseChoice=1")
+                        if (not sgrule['port_range_min']):
+                            continue
+                        elif (sgrule['port_range_min'] in STATEFUL_ICMP_TYPES):
+                            response = self.nuageclient.restproxy.rest_call(
+                                'DELETE', delete_eg_rule, '')
+                            self.validate(response, 'DELETE', rule,
+                                          sgrule['id'])
+                        elif (sgrule['port_range_min'] not in
+                                  STATEFUL_ICMP_TYPES):
+                            data = {
+                                "ICMPType": sgrule['port_range_min'],
+                                "ICMPCode": sgrule['port_range_max']
+                            }
+                            response = self.nuageclient.restproxy.rest_call(
+                                'PUT', delete_eg_rule, data=data)
+                            self.validate(response, 'PUT', rule, sgrule['id'])
+                elif sgrule['direction'] == 'ingress':
+                    if eg_response[3]:
+                        acllist = eg_response[3]
+                        for key, group in itertools.groupby(
+                                acllist, lambda item: item["parentID"]):
+                            for item in group:
+                                update_rule_list.append(item['ID'])
+                    if in_response[3]:
+                        acllist = in_response[3]
+                        for key, group in itertools.groupby(
+                                acllist, lambda item: item["parentID"]):
+                            for item in group:
+                                delete_rule_list.append(item['ID'])
+                    for rule in update_rule_list:
+                        update_eg_rule =  ("/egressaclentrytemplates/"+rule+
+                                           "?responseChoice=1")
+                        if (not sgrule['port_range_min']):
+                            continue
+                        elif (sgrule['port_range_min'] in STATEFUL_ICMP_TYPES):
+                            data = {
+                                "stateful": True,
+                                "ICMPType": sgrule['port_range_min'],
+                                "ICMPCode": sgrule['port_range_max']
+                            }
+                            response = self.nuageclient.restproxy.rest_call(
+                                'PUT', update_eg_rule, data=data)
+                            self.validate(response, 'PUT', rule, sgrule['id'])
+                        elif (sgrule['port_range_min'] not in
+                                  STATEFUL_ICMP_TYPES):
+                            data = {
+                                "ICMPType": sgrule['port_range_min'],
+                                "ICMPCode": sgrule['port_range_max']
+                            }
+                            response = self.nuageclient.restproxy.rest_call(
+                                'PUT', update_eg_rule, data=data)
+                            self.validate(response, 'PUT', rule, sgrule['id'])
+
+                    for rule in delete_rule_list:
+                        delete_in_rule = ("/ingressaclentrytemplates/"+rule+
+                                          "?responseChoice=1")
+                        if (not sgrule['port_range_min']):
+                            continue
+                        elif (sgrule['port_range_min'] in STATEFUL_ICMP_TYPES):
+                            response = self.nuageclient.restproxy.rest_call(
+                                'DELETE', delete_in_rule, '')
+                            self.validate(response, 'DELETE', rule,
+                                          sgrule['id'])
+                        elif (sgrule['port_range_min'] not in
+                                  STATEFUL_ICMP_TYPES):
+                            data = {
+                                "ICMPType": sgrule['port_range_min'],
+                                "ICMPCode": sgrule['port_range_max']
+                            }
+                            response = self.nuageclient.restproxy.rest_call(
+                                'PUT', delete_in_rule, data=data)
+                            self.validate(response, 'PUT', rule, sgrule['id'])
             except Exception as e:
                 LOG.error(str(e))
 
@@ -212,7 +377,8 @@ def main():
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
     hdlr.setFormatter(formatter)
     LOG.addHandler(hdlr)
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+    LOG.setLevel(logging.INFO)
 
     conf_list = []
     for conffile in conffiles:
